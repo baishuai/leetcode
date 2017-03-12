2e06bf042bec38dd22726ab012d0e7b2a72f4cd7
Bai Shuai <baishuai.io@foxmail.com>
2017-03-12 15:07:37 +0800

    101 hits, 076

--- leetcode/p076/076.go
+++ leetcode/p076/076.go
@@ -1,1 +1,67 @@
 package p076
+
+import "fmt"
+
+/**
+Given a string S and a string T, find the minimum window in S which will contain all the characters in T in complexity O(n).
+
+For example,
+S = "ADOBECODEBANC"
+T = "ABC"
+Minimum window is "BANC".
+
+Note:
+If there is no such window in S that covers all characters in T, return the empty string "".
+
+If there are multiple such windows, you are guaranteed that there will always be only one unique minimum window in S.
+ */
+
+func minWindow(s string, t string) string {
+	tHits := make(map[byte]int)
+	for i := 0; i < len(t); i++ {
+		tHits[t[i]] ++
+	}
+	l, h, hitCounts := 0, 0, 0
+	ml, mh, mLen := 0, 0, len(s)+1
+	for h < len(s) || hitCounts >= len(t) {
+
+		if hitCounts < len(t) {
+			for h < len(s) {
+				if _, ok := tHits[s[h]]; ok {
+					break
+				}
+				h++
+			}
+			if h < len(s) {
+				if tHits[s[h]] > 0 {
+					hitCounts++
+				}
+				tHits[s[h]]--
+			}
+			h++
+		} else {
+			if v, ok := tHits[s[l]]; ok {
+				if v >= 0 {
+					hitCounts--
+				}
+				tHits[s[l]]++
+				l++
+			}
+			for l < len(s) {
+				if _, ok := tHits[s[l]]; ok {
+					break
+				}
+				l++
+			}
+		}
+		if hitCounts == len(t) && (h-l) < mLen {
+			ml, mh = l, h
+			mLen = h - l
+			fmt.Println("find", l, h, string([]byte(s)[l:h]))
+		}
+		if mh-ml == len(t) {
+			break
+		}
+	}
+	return string([]byte(s)[ml:mh])
+}
--- leetcode/p076/076_test.go
+++ leetcode/p076/076_test.go
@@ -1,1 +1,27 @@
 package p076
+
+import "testing"
+
+func TestExample0(t *testing.T) {
+	if ans := minWindow("ADOBECODEBANC", "ABC"); ans != "BANC" {
+		t.Error("error anwser", ans)
+	}
+}
+
+func TestExample1(t *testing.T) {
+	if ans := minWindow("A", "A"); ans != "A" {
+		t.Error("error anwser", ans)
+	}
+}
+
+func TestExample2(t *testing.T) {
+	if ans := minWindow("Aqwertyuiofhddfg", "Amt"); ans != "" {
+		t.Error("error anwser", ans)
+	}
+}
+
+func TestExample3(t *testing.T) {
+	if ans := minWindow("cabwefgewcwaefgcf", "cae"); ans != "cwae" {
+		t.Error("error anwser", ans)
+	}
+}
