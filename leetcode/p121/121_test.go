7b148e0ff79ce8e9872c65a2c516d468c977bbcf
Bai Shuai <baishuai.io@foxmail.com>
2017-03-14 23:35:01 +0800

    107 hits,  p121

---
+++ leetcode/p121/121_test.go
@@ -0,0 +1,17 @@
+package p121
+
+import "testing"
+
+func test(t *testing.T, prices []int, exp int) {
+	if ans := maxProfit(prices); ans != exp {
+		t.Fatal("error answer")
+	}
+}
+
+func TestEXample0(t *testing.T) {
+	test(t, []int{7, 1, 5, 3, 6, 4}, 5)
+}
+
+func TestExample1(t *testing.T) {
+	test(t, []int{7, 6, 4, 3, 1}, 0)
+}
