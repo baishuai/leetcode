11ce6b25c81ebdd2496d3cef34175fa7df21f80a
Bai Shuai <baishuai.io@foxmail.com>
2017-03-14 23:53:18 +0800

    108 hits,  p122

---
+++ leetcode/p122/122.go
@@ -0,0 +1,21 @@
+package p122
+
+/**
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete as many transactions as you like
+ (ie, buy one and sell one share of the stock multiple times).
+  However, you may not engage in multiple transactions at the same time
+   (ie, you must sell the stock before you buy again).
+ */
+
+func maxProfit(prices []int) int {
+	mProfit := 0
+	for i := 1; i < len(prices); i++ {
+		diff := prices[i] - prices[i-1]
+		if diff > 0 {
+			mProfit += diff
+		}
+	}
+	return mProfit
+}
