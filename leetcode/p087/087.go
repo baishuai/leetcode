a6db85bd55066bf6976cebc14f9353233dc23d93
Bai Shuai <baishuai.io@foxmail.com>
2017-03-17 11:35:20 +0800

    115 hits, p87

--- leetcode/p087/087.go
+++ leetcode/p087/087.go
@@ -1,7 +1,40 @@
 package p087
 
 
-
 func isScramble(s1 string, s2 string) bool {
+	if s1 == s2 {
+		return true
+	} else if len(s1) != len(s2) {
+		return false
+	}
+	return isScrambleBytes([]byte(s1), []byte(s2))
+}
 
+func isScrambleBytes(bs1 []byte, bs2 []byte) bool {
+	if string(bs1) == string(bs2) {
+		return true
+	}
+
+	letters := make([]int, 26)
+	for i := 0; i < len(bs2); i++ {
+		letters[bs1[i]-'a']++
+		letters[bs2[i]-'a']--
+	}
+	for i := 0; i < 26; i++ {
+		if letters[i] != 0 {
+			return false
+		}
+	}
+
+	for i := 1; i < len(bs2); i++ {
+		if isScrambleBytes(bs1[0:i], bs2[0:i]) &&
+			isScrambleBytes(bs1[i:], bs2[i:]) {
+			return true
+		}
+		if isScrambleBytes(bs1[0:i], bs2[len(bs2)-i:]) &&
+			isScrambleBytes(bs1[i:], bs2[0:len(bs2)-i]) {
+			return true
+		}
+	}
+	return false
 }
--- leetcode/p087/087_test.go
+++ leetcode/p087/087_test.go
@@ -1,1 +1,17 @@
 package p087
+
+import "testing"
+
+func test(t *testing.T, s1, s2 string, exp bool) {
+	if ans := isScramble(s1, s2); ans != exp {
+		t.Fatal("error", ans)
+	}
+}
+
+func TestExample(t *testing.T) {
+	test(t, "great", "rgtae", true)
+}
+
+func TestExtra(t *testing.T) {
+	test(t, "xstjzkfpkggnhjzkpfjoguxvkbuopi", "xbouipkvxugojfpkzjhnggkpfkzjts", true)
+}
