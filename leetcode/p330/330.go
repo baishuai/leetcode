5e8a8d707b308a1384730bcba4519ca4183cac35
Bai Shuai <baishuai.io@foxmail.com>
2017-04-13 10:05:26 +0800

    183 hits, p330

---
+++ leetcode/p330/330.go
@@ -0,0 +1,40 @@
+package p330
+
+/**
+Given a sorted positive integer array nums and an integer n,
+ add/patch elements to the array such that any number in range [1, n] inclusive
+ can be formed by the sum of some elements in the array. Return the minimum number of patches required.
+
+Example 1:
+nums = [1, 3], n = 6
+Return 1.
+
+Combinations of nums are [1], [3], [1,3], which form possible sums of: 1, 3, 4.
+Now if we add/patch 2 to nums, the combinations are: [1], [2], [3], [1,3], [2,3], [1,2,3].
+Possible sums are 1, 2, 3, 4, 5, 6, which now covers the range [1, 6].
+So we only need 1 patch.
+
+Example 2:
+nums = [1, 5, 10], n = 20
+Return 2.
+The two patches can be [2, 4].
+
+Example 3:
+nums = [1, 2, 2], n = 5
+Return 0.
+
+ */
+
+func minPatches(nums []int, n int) int {
+	form, adds, ix := 1, 0, 0
+	for form <= n {
+		if ix < len(nums) && nums[ix] <= form {
+			form += nums[ix]
+			ix++
+		} else {
+			form += form
+			adds++
+		}
+	}
+	return adds
+}
---
+++ leetcode/p330/330_test.go
@@ -0,0 +1,18 @@
+package p330
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test0(t *testing.T) {
+	assert.Equal(t, 1, minPatches([]int{1, 3}, 6))
+}
+
+func Test1(t *testing.T) {
+	assert.Equal(t, 2, minPatches([]int{1, 5, 10}, 20))
+}
+
+func Test2(t *testing.T) {
+	assert.Equal(t, 0, minPatches([]int{1, 2, 2}, 5))
+}
