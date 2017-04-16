5f2c396ce8eb960d28cd42fd6b266997ee6bb731
Bai Shuai <baishuai.io@foxmail.com>
2017-04-16 17:19:38 +0800

    p023, add test

--- leetcode/p023/023_test.go
+++ leetcode/p023/023_test.go
@@ -1,3 +1,26 @@
 package p023
 
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
 
+func Test0(t *testing.T) {
+	t1 := &ListNode{Val: 1}
+	t2 := &ListNode{Val: 2}
+	t3 := &ListNode{Val: 3}
+	t4 := &ListNode{Val: 4}
+	t5 := &ListNode{Val: 5}
+
+	lists := []*ListNode{t1, t2, t3, t4, t5}
+
+	list := mergeKLists(lists)
+
+	assert.True(t, list != nil)
+	i := 1
+	for list != nil {
+		assert.Equal(t, i, list.Val)
+		i++
+		list = list.Next
+	}
+}
