d13ce246b08a4c006173b2380ba24c5529c7079a
Bai Shuai <baishuai.io@foxmail.com>
2017-04-19 08:51:41 +0800

    195 hits, p233

---
+++ leetcode/p233/233.go
@@ -0,0 +1,31 @@
+package p233
+
+/**
+Given an integer n, count the total number of digit 1 appearing in all non-negative
+ integers less than or equal to n.
+
+For example:
+Given n = 13,
+Return 6, because digit 1 occurred in the following numbers: 1, 10, 11, 12, 13.
+ */
+
+func countDigitOne(n int) int {
+	if n <= 0 {
+		return 0
+	} else if n < 10 {
+		return 1
+	}
+	prod, pow := 1, 0
+	for n/prod >= 10 {
+		prod *= 10
+		pow ++
+	}
+	ones := pow * prod / 10
+	ones = ones * (n / prod)
+	if n/prod >= 2 {
+		ones += prod
+	} else {
+		ones += n - prod + 1
+	}
+	return ones + countDigitOne(n%prod)
+}
---
+++ leetcode/p233/233_test.go
@@ -0,0 +1,23 @@
+package p233
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test0(t *testing.T) {
+	assert.Equal(t, 1, countDigitOne(3))
+}
+
+func Test1(t *testing.T) {
+	assert.Equal(t, 6, countDigitOne(13))
+}
+
+func Test2(t *testing.T) {
+	assert.Equal(t, 161, countDigitOne(301))
+}
+
+func Test3(t *testing.T) {
+	assert.Equal(t, 0, countDigitOne(-1))
+	assert.Equal(t, 0, countDigitOne(0))
+}
