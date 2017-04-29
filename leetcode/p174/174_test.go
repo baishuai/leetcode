74f2b6759f25b2dc2776bc940811bb2e2e97e300
Bai Shuai <baishuai.io@foxmail.com>
2017-04-29 22:01:03 +0800

    211 hits, p174

---
+++ leetcode/p174/174_test.go
@@ -0,0 +1,17 @@
+package p174
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test0(t *testing.T) {
+	du := [][]int{{1, -3, 3}, {0, -2, 0}, {-3, -3, -3}}
+
+	assert.Equal(t, 3, calculateMinimumHP(du))
+}
+
+func Test1(t *testing.T) {
+	du := [][]int{{10}}
+	assert.Equal(t, 1, calculateMinimumHP(du))
+}
