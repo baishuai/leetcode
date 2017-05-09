f3b647831251e4d288a4b3b34d372b0a299dbe21
Bai Shuai <baishuai.io@foxmail.com>
2017-05-09 08:09:50 +0800

    p446

---
+++ leetcode/p446/446_test.go
@@ -0,0 +1,28 @@
+package p446
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test0(t *testing.T) {
+	assert.Equal(t, 7, numberOfArithmeticSlices([]int{2, 4, 6, 8, 10}))
+}
+
+func Test1(t *testing.T) {
+	assert.Equal(t, 13, numberOfArithmeticSlices([]int{2, 4, 6, 8, 10, 11, 12}))
+}
+
+func Test2(t *testing.T) {
+	assert.Equal(t, 24, numberOfArithmeticSlices([]int{2, 4, 6, 8, 10, 10, 10, 10}))
+}
+
+func Test3(t *testing.T) {
+	assert.Equal(t, 2, numberOfArithmeticSlices([]int{2, 2, 3, 4}))
+}
+
+func Test4(t *testing.T) {
+	assert.Equal(t, 0, numberOfArithmeticSlices([]int{}))
+	assert.Equal(t, 0, numberOfArithmeticSlices([]int{1}))
+	assert.Equal(t, 0, numberOfArithmeticSlices([]int{1, 2}))
+}
