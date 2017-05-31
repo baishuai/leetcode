7d70d9296e2c618dab55b6bbca0f8bde2eb96eb9
baishuai <baishuai.io@gmail.com>
2017-05-31 13:45:54 +0800

    update test.sh

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ echo "" > coverage.txt
 
 for d in $(go list ./... | grep -v vendor); do
     echo $d
-    go test -race -coverprofile=profile.out -covermode=atomic $d
+    go test -coverprofile=profile.out -covermode=atomic $d
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
         rm profile.out
