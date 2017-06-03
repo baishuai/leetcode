70967df363bc63bb0088b9de9ed3cb561f567c8e
baishuai <baishuai.io@gmail.com>
2017-06-03 12:50:24 +0800

    add test

--- algorithms/p004/004_test.go
+++ algorithms/p004/004_test.go
@@ -4,24 +4,81 @@ import (
 	"testing"
 )
 
-func TestSame(t *testing.T)  {
+func TestSame(t *testing.T) {
 	nums1 := []int{2, 2}
-	nums2 := []int{2,2}
+	nums2 := []int{2, 2}
 
 	mean := findMedianSortedArrays(nums1, nums2)
 
-	if mean != 2{
+	if mean != 2 {
 		t.Fatal("error answer")
 	}
 }
 
-func TestMean(t *testing.T)  {
+func TestMean(t *testing.T) {
 	nums1 := []int{1, 3}
 	nums2 := []int{2}
 
 	mean := findMedianSortedArrays(nums1, nums2)
 
-	if mean != 2{
+	if mean != 2 {
+		t.Fatal("error answer")
+	}
+}
+
+
+func Test0(t *testing.T) {
+	nums1 := []int{1, 3}
+	nums2 := []int{1,2,3}
+
+	mean := findMedianSortedArrays(nums1, nums2)
+
+	if mean != 2 {
+		t.Fatal("error answer")
+	}
+}
+
+
+func Test1(t *testing.T) {
+	nums1 := []int{1}
+	nums2 := []int{1,2,3}
+
+	mean := findMedianSortedArrays(nums1, nums2)
+
+	if mean != 1.5 {
+		t.Fatal("error answer")
+	}
+}
+
+
+func Test2(t *testing.T) {
+	nums2 := []int{3}
+	nums1 := []int{1,2,3}
+
+	mean := findMedianSortedArrays(nums1, nums2)
+	if mean != 2.5 {
+		t.Fatal("error answer")
+	}
+}
+
+
+func Test3(t *testing.T) {
+	nums2 := []int{3}
+	nums1 := []int{1,2,3,4,5}
+
+	mean := findMedianSortedArrays(nums1, nums2)
+	if mean != 3 {
+		t.Fatal("error answer")
+	}
+}
+
+
+func Test4(t *testing.T) {
+	nums2 := []int{2}
+	nums1 := []int{1,2,3,4}
+
+	mean := findMedianSortedArrays(nums1, nums2)
+	if mean != 2 {
 		t.Fatal("error answer")
 	}
 }
--- algorithms/p073/073_test.go
+++ algorithms/p073/073_test.go
@@ -1,1 +1,73 @@
 package p073
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test0(t *testing.T) {
+	mat := [][]int{
+		{1, 1, 1},
+		{1, 0, 1},
+		{1, 1, 0},
+	}
+
+	setZeroes(mat)
+	exp := [][]int{
+		{1, 0, 0},
+		{0, 0, 0},
+		{0, 0, 0},
+	}
+
+	assert.Equal(t, exp, mat)
+}
+
+
+func Test1(t *testing.T) {
+	mat := [][]int{
+		{1, 1, 1},
+		{0, 0, 1},
+		{1, 1, 0},
+	}
+
+	setZeroes(mat)
+	exp := [][]int{
+		{0, 0, 0},
+		{0, 0, 0},
+		{0, 0, 0},
+	}
+
+	assert.Equal(t, exp, mat)
+}
+
+
+func Test2(t *testing.T) {
+	mat := [][]int{
+		{1, 0, 1},
+		{1, 0, 1},
+		{1, 1, 0},
+	}
+
+	setZeroes(mat)
+	exp := [][]int{
+		{0, 0, 0},
+		{0, 0, 0},
+		{0, 0, 0},
+	}
+
+	assert.Equal(t, exp, mat)
+}
+
+
+func Test3(t *testing.T) {
+	mat := [][]int{
+
+	}
+
+	setZeroes(mat)
+	exp := [][]int{
+	}
+
+	assert.Equal(t, exp, mat)
+}
+
