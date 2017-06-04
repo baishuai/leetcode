20e0451cdc65f133105402a3009e36db6ecd2aa2
baishuai <baishuai.io@gmail.com>
2017-06-04 20:13:23 +0800

    update test code p074

--- algorithms/p074/074_test.go
+++ algorithms/p074/074_test.go
@@ -1,1 +1,22 @@
 package p074
+
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test0(t *testing.T) {
+	mat := [][]int{
+		{1, 3, 5, 7},
+		{10, 11, 16, 20},
+		{23, 30, 34, 50},
+	}
+	assert.Equal(t, true, searchMatrix(mat, 3))
+	assert.Equal(t, false, searchMatrix(mat, 4))
+}
+
+func Test1(t *testing.T) {
+	mat := [][]int{}
+	assert.Equal(t, false, searchMatrix(mat, 4))
+}
