eff0690cf0d0039850821493ad80aa5e000cfde5
baishuai <baishuai.io@gmail.com>
2017-06-04 20:16:00 +0800

    update test code p102

--- algorithms/p102/102_test.go
+++ algorithms/p102/102_test.go
@@ -1,7 +1,14 @@
 package p102
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestExample(t *testing.T) {
-	levelOrder(nil)
+	assert.Equal(t, [][]int{}, levelOrder(nil))
+}
+
+func Test1(t *testing.T) {
+	assert.Equal(t, [][]int{{1}, {2, 3}}, levelOrder(&TreeNode{1, &TreeNode{Val: 2}, &TreeNode{Val: 3}}))
 }
