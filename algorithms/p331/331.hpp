a634fb4e973c4fc72de43ac4b5f13765f8645b0b
baishuai <baishuai.io@gmail.com>
2017-07-16 22:22:59 +0800

    p331

--- algorithms/p331/331.hpp
+++ algorithms/p331/331.hpp
@@ -27,31 +27,16 @@ public:
             tokens.push_back(item);
         }
 
-        stack<int> st;
-
-        for (int i = 0; i < tokens.size(); i++) {
-            string t = tokens[i];
-            if (t == "#") {
-                if (st.size() == 0) {
-                    return i == 0 && tokens.size() == 1;
-                } else if (st.top() == 0) {
-                    st.top()++;
-                } else {
-                    st.top()++;
-                    while (st.size() > 0 && st.top() == 2) {
-                        st.pop();
-                    }
-                }
-            } else {
-                if (st.size() > 0) {
-                    st.top()++;
-                } else if (i > 0) {
-                    return false;
-                }
-                st.push(0);
+        int degree = 1;
+        for (auto &t : tokens) {
+            if (--degree < 0) {
+                return false;
+            }
+            if (t != "#") {
+                degree += 2;
             }
         }
-        return st.size() == 0;
+        return degree == 0;
     }
 };
 
