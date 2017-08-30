0a3b40c8f79501216bc1fb9c7d461f2f86417798
baishuai <baishuai.io@gmail.com>
2017-08-30 22:30:08 +0800

    454
    Signed-off-by: baishuai <baishuai.io@gmail.com>

---
+++ algorithms/p454/454.hpp
@@ -0,0 +1,59 @@
+
+#ifndef LEETCODE_454_HPP
+#define LEETCODE_454_HPP
+
+#include <iostream>
+#include <queue>
+#include <algorithm>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+#include <numeric>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+/*
+Given four lists A, B, C, D of integer values,
+ compute how many tuples (i, j, k, l) there are such that A[i] + B[j] + C[k] + D[l] is zero.
+
+To make problem a bit easier, all A, B, C, D have same length of N where 0 ≤ N ≤ 500.
+ All integers are in the range of -228 to 228 - 1 and the result is guaranteed to be at most 231 - 1.
+
+Example:
+
+Input:
+A = [ 1, 2]
+B = [-2,-1]
+C = [-1, 2]
+D = [ 0, 2]
+
+Output:
+2
+
+Explanation:
+The two tuples are:
+1. (0, 0, 0, 1) -> A[0] + B[0] + C[0] + D[1] = 1 + (-2) + (-1) + 2 = 0
+2. (1, 1, 0, 0) -> A[1] + B[1] + C[0] + D[0] = 2 + (-1) + (-1) + 0 = 0
+
+ */
+
+class Solution {
+public:
+    int fourSumCount(vector<int> &A, vector<int> &B, vector<int> &C, vector<int> &D) {
+        unordered_map<int, int> cache;
+        for (auto a : A)
+            for (auto b : B)
+                cache[a + b]++;
+        int cnt = 0;
+        for (auto c : C)
+            for (auto d : D)
+                cnt += cache[0 - c - d];
+        return cnt;
+    }
+};
+
+
+#endif //LEETCODE_454_HPP
---
+++ algorithms/p454/454_test.cpp
@@ -0,0 +1,7 @@
+
+#include "454.hpp"
+#include <gtest/gtest.h>
+
+TEST(p454, example) {
+    Solution s;
+}
